276a1ab5065fa4e664bcbab980ece4b39fa3f2f4
Simon MGK <contact@mgkprod.fr>
2019-11-16 02:15:46 +0100

    Create import commands

---
+++ app/Commands/ImportImagesCommand.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Commands;
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Console\Scheduling\Schedule;
+use LaravelZero\Framework\Commands\Command;
+
+class ImportImagesCommand extends Command
+{
+    /**
+     * The signature of the command.
+     *
+     * @var string
+     */
+    protected $signature = 'import:images';
+
+    /**
+     * The description of the command.
+     *
+     * @var string
+     */
+    protected $description = 'Step 2: Import images from NoelShack';
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $noelshackImages = DB::table('noelshack_images')
+            ->where('image_id', null)
+            ->orderBy('risibank_id', 'asc');
+
+        $bar = $this->output->createProgressBar($noelshackImages->count());
+
+        $noelshackImages->chunk(200, function ($chunk) use ($bar) {
+            $chunk->each(function ($noelshackImage) use ($bar) {
+                $fileName = str_replace('/', '-', $noelshackImage->url);
+
+                try {
+                    $content = file_get_contents('https://image.noelshack.com/fichiers/' . $noelshackImage->url);
+                } catch (\Throwable $th) {
+                    // File was not found on NoelShack, try with the RisiBank cache
+                    try {
+                        $content = file_get_contents('https://risibank.fr/cache/stickers/' . $noelshackImage->risibank_cache_url);
+                    } catch (\Throwable $th) {
+                        // File was not found :sad:
+                        DB::table('noelshack_images')
+                            ->where('id', $noelshackImage->id)
+                            ->delete();
+
+                        return;
+                    }
+                }
+
+                if (Storage::put($fileName, $content, 'public')) {
+                    $imageId = DB::table('images')
+                        ->insertGetId([
+                            'name' => $fileName,
+                            'path' => $fileName,
+                            'size' => Storage::size($fileName),
+                            'created_at' => now(),
+                            'updated_at' => now(),
+                        ]);
+
+                    DB::table('noelshack_images')
+                        ->where('id', $noelshackImage->id)
+                        ->update([
+                            'image_id' => $imageId,
+                            'updated_at' => now(),
+                        ]);
+                }
+
+                $bar->advance();
+            });
+        });
+
+        $bar->finish();
+    }
+
+    /**
+     * Define the command's schedule.
+     *
+     * @param  \Illuminate\Console\Scheduling\Schedule $schedule
+     * @return void
+     */
+    public function schedule(Schedule $schedule): void
+    {
+        // $schedule->command(static::class)->everyMinute();
+    }
+}
---
+++ app/Commands/ImportRisiBankCommand.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Commands;
+
+use Goutte\Client;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Console\Scheduling\Schedule;
+use LaravelZero\Framework\Commands\Command;
+
+class ImportRisiBankCommand extends Command
+{
+    /**
+     * The signature of the command.
+     *
+     * @var string
+     */
+    protected $signature = 'import:risibank';
+
+    /**
+     * The description of the command.
+     *
+     * @var string
+     */
+    protected $description = 'Step 1: Import urls from RisiBank';
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $failedTries = 0;
+        $triesTreshold = 50;
+
+        // Get latest id
+        $id = DB::table('noelshack_images')
+            ->select('risibank_id')
+            ->orderBy('risibank_id', 'desc')
+            ->first()
+            ->risibank_id ?? 0;
+
+        $this->info('Resuming from #' . $id);
+
+        $client = new Client();
+
+        while ($failedTries < $triesTreshold) {
+            ++$id;
+
+            $crawler = $client->request('GET', 'https://risibank.fr/stickers/' . $id . '-' . uniqid());
+
+            if (
+                $client->getResponse()->getStatus() != 200
+                || $crawler->filter('input[name="search"]')->count()
+            ) {
+                ++$failedTries;
+                $this->line('#' . $id . ' does not exists (failed: ' . $failedTries . ')');
+                continue;
+            } else {
+                // Reset failed counter
+                $failedTries = 0;
+            }
+
+            $noelshackUrl = $crawler->filter('tbody>tr')
+                ->eq(1)
+                ->filter('td')
+                ->eq(1)
+                ->text();
+            $noelshackUrl = trim($noelshackUrl);
+
+            $risibankCacheUrl = $crawler->filter('.img-preview-big')
+                ->attr('src');
+            $risibankCacheUrl = trim($risibankCacheUrl);
+            $risibankCacheUrl = str_replace('https://risibank.fr/cache/stickers/', '', $risibankCacheUrl);
+
+            DB::table('noelshack_images')->insert([
+                'url' => $noelshackUrl,
+                'risibank_id' => $id,
+                'risibank_cache_url' => $risibankCacheUrl,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+
+            $this->line('#' . $id . ' saved into database');
+        }
+    }
+
+    /**
+     * Define the command's schedule.
+     *
+     * @param  \Illuminate\Console\Scheduling\Schedule $schedule
+     * @return void
+     */
+    public function schedule(Schedule $schedule): void
+    {
+        // $schedule->command(static::class)->everyMinute();
+    }
+}
